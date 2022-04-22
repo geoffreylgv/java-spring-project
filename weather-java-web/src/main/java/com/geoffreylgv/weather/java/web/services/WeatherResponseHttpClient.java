e1bda59863d71624f6c8542cdd7dfb087af51227
geoffreylgv <evanomarkez@gmail.com>
2022-04-22 17:35:19 +0000

    fetch weather data httpclient created in service package

---
+++ weather-java-web/src/main/java/com/geoffreylgv/weather/java/web/services/WeatherResponseHttpClient.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.geoffreylgv.weather.java.web.services;
+
+import com.geoffreylgv.weather.java.web.model.WeatherResponse;
+import java.util.LinkedList;
+import java.util.List;
+import lombok.SneakyThrows;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author geoffreylgv
+ */
+@Service
+public class WeatherResponseHttpClient {
+
+    private String API_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
+    private String API_KEY = "";
+
+    //media data head type
+    private void mediaDataHead(HttpRequestBase httpRequestBase) {
+        httpRequestBase.setHeader("Accept", "application/json");
+        httpRequestBase.setHeader("Content-type", "application/json;charset=UTF-8");
+    }
+
+    //api full url
+    private String getFullUrl(String uri) {
+        String url = API_URL + uri + "&appid=" + API_KEY;
+        return url;
+    }
+
+    //get weather data response (all data)
+    @SneakyThrows
+    public List<WeatherResponse> getWeatherResponseByCity(String city) {
+        List<WeatherResponse> weList = new LinkedList<>();
+
+        HttpGet httpGet = new HttpGet(getFullUrl(city));
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+
+        return weList;
+    }
+}
