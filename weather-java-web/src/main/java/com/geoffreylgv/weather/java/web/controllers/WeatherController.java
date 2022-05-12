2f9936d097803280b2a1b465e30807a2e7007277
geoffreylgv <evanomarkez@gmail.com>
2022-05-12 11:18:00 +0000

    weathercontroller to get weather data(new way to load the weather data)

---
+++ weather-java-web/src/main/java/com/geoffreylgv/weather/java/web/controllers/WeatherController.java
@@ -0,0 +1,14 @@
+package com.geoffreylgv.weather.java.web.controllers;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.OK;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/weather")
+public class WeatherController {
+
+}
